477920709ef64408040fe1418e9b03c90058b208
arifzaidpp <arifzaidaiju@gmail.com>
2024-03-16 10:24:14 +0530

    job Done

---
+++ script.js
@@ -0,0 +1,11 @@
+function buttonclick(val) {
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+function cleardisplay(params) {
+    document.getElementById("screen").value=""
+}
+function equalClick(params) {
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+}
